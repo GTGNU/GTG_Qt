f79309bd65d438f074990a710dcfa4e303c4a6ab
Axujen <axujen@gmail.com>
2013-03-21 12:24:54 +0000

    Use new script from "https://docs.google.com/document/d/1gXds5AF7W5x1SrepwZp61jZwiXSH7hv1EmR3P-MIaLk/edit?pli=1"
    Also using printf instead of cout now because i can.

--- game.cpp
+++ game.cpp
@@ -2,43 +2,131 @@
 // #include <Windows.h>
 // We do not endorse proprietary operating systems.
 #include <pthread.h> // Free and Open Source software is always better
+#include <cstdio> // Because screw iostream
 
 using std::cout;
+using std::printf;
 // using std::cin; // What cin?
 using std::endl;
 
 int main()
 {
-  cout << "You are Richard Stallman, the founder of the FSF and the GNU project.\nYou are an advocate of free software and the GPL license. Steven Ballmer is the C.E.O. of Micro$hills.\nHe does not share your philosophies about freedom and is an advocate of evil proprietary software.\n" << endl; 
-  sleep(2);
-  cout << "Stallman, wake up!" << endl;
-  sleep(2);
-  cout << "You wake up covered in stale cheetos from the a long night of checking troll emails from /g/." << endl;
-  sleep(2);
-  cout << "You look at the clock, it's 2 in the afternoon." << endl;
-  sleep(2);
-  cout << "You blink twice to clear your vision and shota-Linus appears in your field of view." << endl;
-  sleep(2);
-  cout << "\"What is it now, Linus? This better be fucking important! You know I need my beardy sleep!\"" << endl;
-  sleep(2);
-  cout << "\"THIS IS IMPORTANT! Ubuntu has gone proprietary!\" shota-Linus' girly voice rang in your ears." << endl;
-  sleep(2);
-  cout << "\"WHAT!?\" you exclaim, suddenly fully awake, \"THAT\'S AGAINST THE LICENSE!\"" << endl;
-  sleep(2);
-  cout << "\"I couldn't believe it either,\" shota-Linus squeaked, \"but it's true! They found a loophole! They're distributing proprietary Linux to the masses!\"" << endl;
-  sleep(2);
-  cout << "\"GNU...plus...Linux. Anyways, what the hell do they think this is? BSD!?\" You said" << endl;
-  sleep(2);
-  cout << "Shota-Linus rolls you out of bed. You then command him to boot up your Lemote Yeeloong." << endl;
-  sleep(2);
-  cout << "He protests that it will take forever, but he does it anyways." << endl;
-  sleep(2);
-  cout << "After 30 minutes of waiting, the ancient machine displays a faint gNewSense logo on its greasy display." << endl;
-  sleep(2);
-  cout << "You then spend the next 3 hours checking emails, while shota-Linus tugs on your beard." << endl;
-  sleep(2);
-  cout << "The constant tugging on your beard eventually annoys you, and you" << endl; 
+	const char *intro = "RMS is a coder out to destroy proprietary software. Balmer (the anti-hero) dose not share RMS's philosophy and aims to silence RMS."
+		"\n########intro#########"
+		"\n*RMS (you) are working at MIT, you give speeches about the use of free and open software. Ballmer is looking for a new supervisor. He claims that he supports the open source movement. "
+		"\n*The uni begins to loose money due to new product and (you) begin to worry that people will not be able to hear your talks and therefore not be able to spread you believes about 'FOSS'."
+		"\n*(you) are back in the lab, sulking over this tragic set back. Errors start appearing on your terminal, NVIDIA. you try to desperately to fix them. coding as fast as you can but to no avail." 
+		"\n*falling over from exhaustion, drenched in sweat, you see a figure in the door way but, you cannot see who it is. your vision is blurry and you begin to black out. there is a laugh coming from some where but you cannot place it. #black out#"
+		"\n*you wake up in a hospital bed with wires and tubes sticking out of you. A cyborg joke crosses your mind but there are more important things to think about. who was that person you saw? who was laughing? why were there so many errors?"
+		"\n*knowing what failed (the NVIDIA (what ever we decides that failed)) you decide that you will get to the bottom of it. even if you have to go all the way to the top of NVIDIA."
+		"\n*several calls and emails later (and talking to people in your lab) you discover that it was not NVIDIA's fault. Maybe its their rivals ATI who are to blame."
+		"\n*while working in the lab again you hear the subtle buzz or a pager by the door and once again see the shadowy figure. you quickly look up and shout 'who is it?!'. realizing they have been spotted the figure darts out of the door way. you quickly get up and chase after them but they disappear before you make it out of the hallway. walking back to the lab you notice a key card on the ground it reads 'ATI HQ' across the top."
+		"\n*you decide to check it out and while at there you meet Logan from teksyndicate. after talking to some people you discover that ballmer has turned to proprietary software. after more research you find that he has bought out ATI.";
 
-  
+	// printf("%s\n\n", intro);
+  // sleep(2);
+  // cout << "Stallman, wake up!" << endl;
+  // sleep(2);
+  // cout << "You wake up covered in stale cheetos from the a long night of checking troll emails from /g/." << endl;
+  // sleep(2);
+  // cout << "You look at the clock, it's 2 in the afternoon." << endl;
+  // sleep(2);
+  // cout << "You blink twice to clear your vision and shota-Linus appears in your field of view." << endl;
+  // sleep(2);
+  // cout << "\"What is it now, Linus? This better be fucking important! You know I need my beardy sleep!\"" << endl;
+  // sleep(2);
+  // cout << "\"THIS IS IMPORTANT! Ubuntu has gone proprietary!\" shota-Linus' girly voice rang in your ears." << endl;
+  // sleep(2);
+  // cout << "\"WHAT!?\" you exclaim, suddenly fully awake, \"THAT\'S AGAINST THE LICENSE!\"" << endl;
+  // sleep(2);
+  // cout << "\"I couldn't believe it either,\" shota-Linus squeaked, \"but it's true! They found a loophole! They're distributing proprietary Linux to the masses!\"" << endl;
+  // sleep(2);
+  // cout << "\"GNU...plus...Linux. Anyways, what the hell do they think this is? BSD!?\" You said" << endl;
+  // sleep(2);
+  // cout << "Shota-Linus rolls you out of bed. You then command him to boot up your Lemote Yeeloong." << endl;
+  // sleep(2);
+  // cout << "He protests that it will take forever, but he does it anyways." << endl;
+  // sleep(2);
+  // cout << "After 30 minutes of waiting, the ancient machine displays a faint gNewSense logo on its greasy display." << endl;
+  // sleep(2);
+  // cout << "You then spend the next 3 hours checking emails, while shota-Linus tugs on your beard." << endl;
+  // sleep(2);
+  // cout << "The constant tugging on your beard eventually annoys you, and you" << endl; 
+	//
+	// ==================================================================================================================================================================================
+	//
+	// New script
+	//
+	printf("%s\n", intro);
+	sleep(2);
+	printf("Stallman, wake up!");
+	sleep(2);
+	printf("\n>you wakes up");
+	sleep(2);
+	printf("\nYou find yourself covered in stale cheetos from a long night of reading troll emails from /g/.");
+	sleep(2);
+	printf("\n>you look at the clock, it's 2 in the afternoon");
+	sleep(2);
+	printf("\n'What the hell! I'm not supposed to wake up for another 2 hours and twenty minutes, this better be fucking important!' You say.");
+	sleep(2);
+	printf("\n>you shift over to look at shota-linus who woke you up."); 
+	sleep(2);
+	printf("\n'IT IS IMPORTANT! Ubuntu's gone proprietary.' He says");
+	sleep(2);
+	printf("\n'WHAT! That's against the liscense!'");
+	sleep(2);
+	printf("\nThey found a loophole somehow. They're going to bring linux to the masses in proprietary form!");
+	sleep(2);
+	printf("\nWhat do they think this is, BSD?");
+	sleep(2);
+
+	printf("\n\n");
+
+	printf("\n>shota-linus helps RMS out of bed.");
+	sleep(2);
+	printf("\n'Quick, Linus boot up my Yeemote!' You say.");
+	sleep(2);
+	printf("\n'But that'll take forever!' He replies.");
+	sleep(2);
+	printf("\n>RMS boots it up anyway.");
+	sleep(2);
+	printf("\n>after 30 minutes, the ancient machine dimly displays the gNewSense logo.");
+	sleep(2);
+	printf("\n>RMS spends the next 3 hours checking emails.");
+	sleep(2);
+	printf("\nshota-linus tugs your beard.");
+	sleep(2);
+	printf("\n'They're uploading the proprietary version in an hour!' He says"); 
+	sleep(2);
+	printf("\nYou look at your inbox and, despite the fact that you still have over 300 unread e-mails, you nod your head.");
+	sleep(2);
+	printf("\n'Start up your peice of junk, then.'");
+	sleep(2);
+	printf("\nshota-linus starts up the laptop that some company paid him 10 dollars to write a good review about. It boots in 7 seconds, and the walk out the door.");
+	sleep(2);
+	printf("\n'Hey, Stallman, if we're such great programmers and developers, why do we do most of our stuff on shitty laptops?'");
+	sleep(2);
+	printf("\n>you remain silent.");
+	sleep(2);
+
+	printf("\n");
+
+	printf("\n>starts in RMS's apartment.");
+	sleep(2);
+	printf("\n>announcement of Ubuntu kicks every thing off.");
+	sleep(2);
+	printf("\n>stallman decides to take action against the makers of ubuntu.");
+	sleep(2);
+	printf("\n>you go to the convention.");
+	sleep(2);
+	printf("\n>see all the ubuntu platforms.");
+	sleep(2);
+	printf("\n>you leave.");
+	sleep(2);
+	printf("\n>you return to the apartment.");
+	sleep(2);
+	printf("\n>discus what happened and wonder what should be done about it.");
+	sleep(2);
+	printf("\n>");
   return 0;
 }
