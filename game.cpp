e2d4a4d4c23a4b7dae8f14d14b006bc727492e7a
Axujen <axujen@gmail.com>
2013-03-21 11:43:18 +0000

    Fix sleep.
    You need pthread to compile now, im not sure if its available in windows
    but it should.
    
    sleep now works, lel at Sleep(500).

--- game.cpp
+++ game.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 // #include <Windows.h>
 // We do not endorse proprietary operating systems.
+#include <pthread.h> // Free and Open Source software is always better
 
 using std::cout;
 // using std::cin; // What cin?
@@ -9,33 +10,33 @@ using std::endl;
 int main()
 {
   cout << "You are Richard Stallman, the founder of the FSF and the GNU project.\nYou are an advocate of free software and the GPL license. Steven Ballmer is the C.E.O. of Micro$hills.\nHe does not share your philosophies about freedom and is an advocate of evil proprietary software.\n" << endl; 
-  // Sleep(5000)
+  sleep(2);
   cout << "Stallman, wake up!" << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "You wake up covered in stale cheetos from the a long night of checking troll emails from /g/." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "You look at the clock, it's 2 in the afternoon." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "You blink twice to clear your vision and shota-Linus appears in your field of view." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "\"What is it now, Linus? This better be fucking important! You know I need my beardy sleep!\"" << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "\"THIS IS IMPORTANT! Ubuntu has gone proprietary!\" shota-Linus' girly voice rang in your ears." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "\"WHAT!?\" you exclaim, suddenly fully awake, \"THAT\'S AGAINST THE LICENSE!\"" << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "\"I couldn't believe it either,\" shota-Linus squeaked, \"but it's true! They found a loophole! They're distributing proprietary Linux to the masses!\"" << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "\"GNU...plus...Linux. Anyways, what the hell do they think this is? BSD!?\" You said" << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "Shota-Linus rolls you out of bed. You then command him to boot up your Lemote Yeeloong." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "He protests that it will take forever, but he does it anyways." << endl;
-  // Sleep(1000);
+  sleep(2);
   cout << "After 30 minutes of waiting, the ancient machine displays a faint gNewSense logo on its greasy display." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "You then spend the next 3 hours checking emails, while shota-Linus tugs on your beard." << endl;
-  // Sleep(500);
+  sleep(2);
   cout << "The constant tugging on your beard eventually annoys you, and you" << endl; 
 
   
