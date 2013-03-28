f1a09a353c88c1d50c108f88b4c750806816e245
Antonio Montes <antonio_montes@outlook.com>
2013-03-28 15:09:15 -0700

    Fixing mistake
    Supposed to be *map

--- Code/GTG/GTGPlayer.h
+++ Code/GTG/GTGPlayer.h
@@ -20,7 +20,7 @@ public:
 
   GTGPlayer(); ~GTGPlayer();
   void Construct(); void Destruct();
-  void Frame(SDLTimer *timer, bool left, bool right, bool up, bool down, GTGMap *mapfloat);
+  void Frame(SDLTimer *timer, bool left, bool right, bool up, bool down, GTGMap *map);
   void Spawn(int px, int py);
   //violates the camelCase style, think of better name
   bool is_moving() {return isMoving;}
