1544dae7b9d31d240e19ce329e647a401e9dd9a3
xcv_ <this@notmy.email>
2013-04-03 22:29:34 +0200

    The Qt map loader now respects your freedoms

--- Code/GameScene/MapLoader/NamedObject.h
+++ Code/GameScene/MapLoader/NamedObject.h
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2013 xcv_
+ *
+ * This file is part of Grand Theft Gentoo.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * Grand Theft Gentoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grand Theft Gentoo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef NAMEDOBJECT_H
 #define NAMEDOBJECT_H
 
