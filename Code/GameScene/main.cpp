7a191c0f52a38d2520836beac5312b93d986a829
xcv_ <this@notmy.email>
2013-04-06 16:24:16 +0200

    Fixed license Foobar -> Grand Theft Gentoo

--- Code/GameScene/main.cpp
+++ Code/GameScene/main.cpp
@@ -3,7 +3,7 @@
  *
  * This file is part of Grand Theft Gentoo.
  *
- * Foobar is free software: you can redistribute it and/or modify
+ * Grand Theft Gentoo is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation version 3.
  *
