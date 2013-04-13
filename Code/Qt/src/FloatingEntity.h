7cd3cf3f18846aa1e82a6c58b6bca630cbf9e8b7
xcv_ <this@notmy.email>
2013-04-13 16:40:32 +0200

    Forgot git add

---
+++ Code/Qt/src/FloatingEntity.h
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2013 xcv_
+ *
+ * This file is part of Grand Theft Gentoo.
+ *
+ * Foobar is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation version 3.
+ *
+ * Grand Theft Gentoo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Grand Theft Gentoo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef FLOATINGENTITY_H
+#define FLOATINGENTITY_H
+
+#include <QtCore/QList>
+
+#include <QtQuick/QQuickItem>
+
+namespace gtg
+{
+	class FloatingEntity
+		: public QQuickItem
+	{
+		Q_OBJECT
+
+		Q_PROPERTY(
+				QQmlListProperty<gtg::Tile> intersectingTiles
+				READ intersectingTilesQml)
+
+		private:
+
+
+		public:
+
+	}
+}
+
+#endif
