33b9b6efb2613535f1704fc4bcd7056854464ed2
xcv <>
2013-05-02 21:35:02 +0200

    Updated compile.sh

--- game/compile.sh
+++ game/compile.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+shopt -s nullglob
 trap error EXIT
 
 step=""
@@ -60,32 +61,54 @@ DIR=$(dirname $SCRIPT)
 cd "$DIR"
 
 show_n ">> Qt SDK location (leave empty if not using the SDK): "
-read qt_sdk_dir
+read -e qt_sdk_dir
 
+qmake_bins=()
+
+# Find all qmake binaries in the specified location
 if [ -z "$qt_sdk_dir" ]
 then
-	qmake_bin="$(which qmake)"
+	qmake_bins=("$(qtchooser -qt=5 -run-tool=qmake -query QT_INSTALL_BINS)/qmake")
 else
-	i=0
-	for bin in "$qt_sdk_dir"/5.*/*/bin/qmake
+	qmake_dirs="$qt_sdk_dir"/5.*/*/bin
+
+	for qmake_dir in ${qmake_dirs[@]}
 	do
-		if [ $i = 0 ]
+		qmake_path="$qmake_dir/qmake"
+		if [ -e "$qmake_path" ]
 		then
-			qmake_bin="$bin"
-			i=$((i+1))
-		else
-			show "### There's more than one qmake binary. Please compile manually." >&2
-			exit_with 1
+			qmake_bins=("${qmake_bins[@]}" "$qmake_path")
 		fi
 	done
-
-	if [ ! -e $bin ]
-	then
-		show "### Could not find qmake ($bin does not exist). Please compile manually" >&2
-		exit_with 1
-	fi
 fi
 
+# Select the binary automatically if only one
+if [ "${#qmake_bins[@]}" = 0 ]
+then
+	# Skip, catch the empty variable below
+	true
+
+elif [ "${#qmake_bins[@]}" = 1 ]
+then
+	qmake_bin=${qmake_bins[0]}
+
+else
+	# Ask the user which one to use
+	show "Please select the qmake binary to use: "
+	select bin in "${qmake_bins[@]}"
+	do
+		qmake_bin="$bin"
+	done
+fi
+
+# Check if it exists
+if [ ! -f "$qmake_bin" ] || [ ! -x "$qmake_bin" ]
+then
+	show "### Could not find qmake ('$qmake_bin' is not an executable file). Please compile manually" >&2
+	exit_with 1
+fi
+
+# Confirm
 show
 show "> cwd: $PWD"
 show "> qmake: $qmake_bin"
@@ -94,6 +117,7 @@ show
 show_n ">> Continue? "
 read ans
 
+# Continue
 if [ "$ans" = "y" ] || [ "$ans" = "Y" ] || [ "$ans" = "yes" ]
 then
 	clean
