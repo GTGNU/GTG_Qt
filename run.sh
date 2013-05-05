39d3ce79d34a73b034ebf448082a39525313e09a
jdoe0 <jdoe@mailinator.com>
2013-05-04 20:30:56 -0500

    run.sh: Fixed binary name when running editor

--- run.sh
+++ run.sh
@@ -16,6 +16,11 @@ argv_help_flag "$@"
 argv_component_arg "$@"
 
 run cd "$component_dir/build/package"
-run ./gtg
+
+if [ $component == "game" ]; then
+	run ./gtg
+elif [ $component == "editor" ]; then
+	run ./editor
+fi
 
 exit_with $?
